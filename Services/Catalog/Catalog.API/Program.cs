811a92ac9b461aaa10d36eb455934c1494c7efea
niv-e <niveliyahu25@gmail.com>
2024-07-17 21:11:08 +0300

    Add api versionning

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,10 @@
+using Asp.Versioning;
+using Asp.Versioning.Conventions;
+using Catalog.API.OpenApi;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,23 +12,63 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen( option => option.OperationFilter<SwaggerDefaultValues>());
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = new QueryStringApiVersionReader();
+}).AddApiExplorer(
+    options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+    });
+
+builder.Services.AddHealthChecks()
+    .AddMongoDb(
+        builder.Configuration["DatabaseSettings:ConnectionString"]!,
+        "Catalog MongoDB health Check",
+        HealthStatus.Degraded);
 
 var app = builder.Build();
 
+var versionSet = app.NewApiVersionSet()
+    .HasApiVersion(1)
+    .HasApiVersion(2)
+    .ReportApiVersions()
+    .Build();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
-}
-
-
 app.UseRouting();
 app.UseStaticFiles();
 app.UseAuthorization();
 app.UseHttpsRedirection();
 app.MapControllers();
+app.MapGet("/hello", () => "Hellow world v1")
+    .WithApiVersionSet(versionSet)
+    .MapToApiVersion(1);
+
+app.MapGet("/hello", () => "Hellow world v2")
+    .WithApiVersionSet(versionSet)
+    .MapToApiVersion(2);
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        var descriptions = app.DescribeApiVersions();
+
+        foreach (var desc in descriptions)
+        {
+            var url = $"/swagger/{desc.GroupName}/swagger.json";
+            var name = desc.GroupName.ToUpperInvariant();
+            options.SwaggerEndpoint(url, name);
+        }
+    });
+    app.UseDeveloperExceptionPage();
+}
 
 app.Run();
+ 
