f2c218874445792c155cbda8702d2b706aa3e40d
niv-e <niveliyahu25@gmail.com>
2024-08-17 20:17:03 +0300

    Add core entities and interface

---
+++ Services/Discount/Discount.Core/Entities/Coupon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Core.Entities;
+
+public record Coupon(string Id, string ProductName, string Description, int Amount)
+{
+}
---
+++ Services/Discount/Discount.Core/Repositories/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+﻿using Discount.Core.Entities;
+
+namespace Discount.Core.Repositories;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(string productName);
+    Task<Coupon> CreateDiscount(Coupon coupon);
+    Task<Coupon> UpdateDiscount(Coupon coupon);
+    Task<Coupon> DeleteDiscount(Coupon coupon);
+}
